a13640781116a36e1f37cf9788df072982dafa21
mahim007 <ashrafulmahim@gmail.com>
2019-08-11 22:28:31 +0600

    hold and roll button's work finished

--- pig-game/app.js
+++ pig-game/app.js
@@ -5,8 +5,43 @@ currentScore = [0,0];
 globalScore = [0,0];
 
 document.querySelector('.dice').style.display='none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-randomNumber = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber);
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    randomNumber = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + randomNumber + '.png';
+    if (randomNumber > 1) {
+        currentScore[currentPlayer] += randomNumber;
+        document.getElementById('current-' + currentPlayer).textContent = currentScore[currentPlayer];
+    }
+    else {
+        nextPlayer();
+    }
+});
 
-document.querySelector('#current-' + currentPlayer).textContent = randomNumber;
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    globalScore[currentPlayer] += currentScore[currentPlayer];
+    document.querySelector('#score-' + currentPlayer).textContent = globalScore[currentPlayer];
+    if(globalScore[currentPlayer] >= 20) {
+        document.querySelector('#name-' + currentPlayer).textContent = 'Winner';
+        document.querySelector('.player-' + currentPlayer + '-panel').classList.toggle('active');
+        document.querySelector('.player-' + currentPlayer + '-panel').classList.add('winner');
+    } else {
+        nextPlayer();
+    }
+    document.querySelector('.dice').style.display = 'none';
+});
+
+function nextPlayer() {
+    currentScore[currentPlayer] = 0;
+    document.querySelector('#current-' + currentPlayer).textContent = currentScore[currentPlayer];
+    document.querySelector('.player-' + currentPlayer + '-panel').classList.toggle('active');
+
+    currentPlayer = currentPlayer === 0 ? 1 : 0;
+    document.querySelector('.player-' + currentPlayer + '-panel').classList.toggle('active');
+}
