2139142cd82f2521e5b122aa4233763d051fbcb6
mahim007 <ashrafulmahim@gmail.com>
2019-08-26 02:32:20 +0600

    item is added to UI

--- budgety/app.js
+++ budgety/app.js
@@ -57,7 +57,9 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputButton: '.add__btn'
+        inputButton: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list'
     };
     return {
         getInput: function () {
@@ -72,6 +74,34 @@ var UIController = (function () {
         },
         getDOMString: function () {
             return DOMStrings;
+        },
+        addListItem: function(obj, type) { console.log('here: ', obj, " type: ", type);
+            var html, newHtml, selector;
+
+            if(type === 'inc') {
+                selector = DOMStrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                newHtml = html.replace('%id%', obj.id);
+                newHtml = newHtml.replace('%description%', obj.description);
+                newHtml = newHtml.replace('%value%', obj.value);
+            } else {
+                selector = DOMStrings.expenseContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description" >%description%</div ><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div></div >';
+                newHtml = html.replace('%id%', obj.id);
+                newHtml = newHtml.replace('%description%', obj.description);
+                newHtml = newHtml.replace('%value%', obj.value);
+            }
+
+            document.querySelector(selector).insertAdjacentHTML('beforeend', newHtml);
+        },
+        clearInputFields: function() {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' + DOMStrings.inputValue);
+            fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(element => {
+                element.value = '';
+            });
+            fieldsArr[0].focus();
         }
     }
 })();
@@ -89,7 +119,9 @@ var controller = (function (budgetCtrl, UICtrl) {
     var ctrlAddItem = function () {
         var input = UICtrl.getInput();
         console.log('input: ', input);
-        budgetCtrl.addItem(input.type, input.description, input.value);
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        UICtrl.addListItem(newItem, input.type);
+        UICtrl.clearInputFields();
     };
 
     return {
