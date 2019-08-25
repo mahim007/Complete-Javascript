272add26e31ac941423a90dbdde56c856c2c030a
mahim007 <ashrafulmahim@gmail.com>
2019-08-25 15:10:19 +0600

    difference between let and var

---
+++ ES6/let-var.js
@@ -0,0 +1,21 @@
+function drivingLicense5(isPassed) {
+    if(isPassed) {
+        var firstName = 'mahim';
+        var yearOfBirth = 1995;
+    }
+
+    console.log(firstName + " born in " + yearOfBirth);
+}
+
+drivingLicense5(true);
+
+function drivingLicense6(isPassed) {
+    if(isPassed) {
+        let firstName = 'mahim';
+        const yearOfBirth = 1995;
+    }
+
+    console.log(firstName + " born in " + yearOfBirth);
+}
+
+drivingLicense6(true);
